ff0a3291afc75a251c686e2684a8399837243ed7
Y.Yamamoto <tuki0918@gmail.com>
2025-05-26 04:36:45 +0900

    Add ThemeWrapper component to manage light and dark themes with type definitions for EagleTheme.

---
+++ src/components/ThemeWrapper.tsx
@@ -0,0 +1,34 @@
+import type { PropsWithChildren } from "react";
+
+export type Theme = "light" | "dark";
+export type EagleTheme =
+  | "LIGHT"
+  | "LIGHTGRAY"
+  | "GRAY"
+  | "DARK"
+  | "BLUE"
+  | "PURPLE";
+
+export const ThemeWrapper = ({
+  theme,
+  children,
+}: PropsWithChildren<{ theme: EagleTheme | string }>) => (
+  <div data-theme={lightOrDark(theme)}>{children}</div>
+);
+
+export default ThemeWrapper;
+
+function lightOrDark(theme: EagleTheme | string): Theme {
+  switch (theme) {
+    case "LIGHT":
+    case "LIGHTGRAY":
+      return "light";
+    case "GRAY":
+    case "BLUE":
+    case "PURPLE":
+    case "DARK":
+      return "dark";
+    default:
+      return "light";
+  }
+}
