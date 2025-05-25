eb01bd5489fba180bb80de30bb1ae4fb18ce571f
Y.Yamamoto <tuki0918@gmail.com>
2025-05-25 21:27:18 +0000

    feat: importの@エイリアス対応 (Vite/TS両方)

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
-import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "./", // for Eagle Plugin
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": "/workspaces/eagle-raw-exif-inspector/src",
+    },
+  },
 });
